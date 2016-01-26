6ab2a70226c0cc7a7e63be5566abaca92201e80f
App Academy Student <appacademy@Apps-Mac-mini-74.local>
2016-01-26 14:40:03 -0500

    Add session information and update view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,16 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
   end
 
   def create
+
     @user = User.new(user_params)
     if @user.save
-      redirect_to success_user(@user)
+      redirect_to :new
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
     end
-  end
-
-  def success
-    @user = User.find(params[:id])
   end
 
   def destroy
@@ -25,7 +21,7 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(
-      :fname, :lname, :password, :password_confirmation, :location, :username, :birth_month, :birth_day, :birth_year 
+      :fname, :lname, :password, :password_confirmation, :location, :username, :birth_month, :birth_day, :birth_year
     )
-
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ActiveRecord::Base
 
-  attr_reader :password
-
   validates :username, :fname, :lname, :password_digest,
     :birthday, :gender, :location, presence: true
   validates :password, confirmation: true, length: { maximum: 6, allow_nil: true }
+  validates :birth_day, inclusion: { in: 1..31 }
+  validates :birth_month, inclusion: { in: 1..12 }
+  validates :birth_year, inclusion: { in: 1900..2016 }
   validates :session_token, presence: true, uniqueness: true
 
-  after_initialize :ensure_session_token
+  after_initialize :ensure_session_token, :ensure_birthday
 
   def self.find_by_credentials(username, password)
     user = User.find_by_username(username)
@@ -25,17 +26,34 @@ class User < ActiveRecord::Base
     self.session_token
   end
 
+  def birth_day=(birth_day)
+    @birth_day = birth_day
+  end
+
+  def birth_year=(birth_year)
+    @birth_year = birth_year
+  end
+
+  def birth_month=(birth_month)
+    @birth_month = birth_month
+  end
+
   def password=(password)
     @password = password
-    this.password_digest = BCrypt::Password.create(password)
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   def is_password?(password)
-    BCrypt::Password.new(this.password_digest).is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
   private
   def ensure_session_token
     self.session_token ||= self.class.generate_session_token
+  end
+
+  def ensure_birthday
+    date_string = [@birth_year, @birth_month, @birth_day].join("-")
+    self.birthday = Date.parse(date_string)
   end
 end
